6c3de68abc776e1115682433f1f1fca8604bf822
Joaquim Leitão <jocaleitao93@gmail.com>
2018-04-05 14:24:43 +0100

    Update brightness script

--- brightness.sh
+++ brightness.sh
@@ -10,9 +10,12 @@ CURRBRIGHT=$(xrandr --current --verbose | grep -m 1 'Brightness:' | cut -f2- -d:
 if [ "$1" = "+" ] && [ $(echo "$CURRBRIGHT < 1" | bc) -eq 1 ]
 then
 xrandr --output $2 --brightness $(echo "$CURRBRIGHT + 0.1" | bc)
+# Set HDMI brightness
+xrandr --output HDMI-0 --brightness $(echo "$CURRBRIGHT + 0.1" | bc)
 elif [ "$1" = "-" ] && [ $(echo "$CURRBRIGHT > 0" | bc) -eq 1 ]
 then
 xrandr --output $2 --brightness $(echo "$CURRBRIGHT - 0.1" | bc)
+xrandr --output HDMI-0 --brightness $(echo "$CURRBRIGHT - 0.1" | bc)
 fi
 
 # TODO: Set keyboard bindings
